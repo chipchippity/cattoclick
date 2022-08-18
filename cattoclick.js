9cc7fa9dae24d2eb2459ebba1f51e4dd70c3c573
chipchippity <40838711+chipchippity@users.noreply.github.com>
2022-08-17 23:30:20 -0700

    Create cattoclick.js

---
+++ cattoclick.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         cattoclick - bring back the old instagram feed
+// @namespace    http://www.squabbled.net
+// @version      0.1
+// @description brings back the old following feed on instagram
+// @author       squabbled
+// @match        https://www.instagram.com/
+// @icon         https://64.media.tumblr.com/e98a83eb58fb4901971de2501d320453/tumblr_inline_nt5la68WyM1r1hsi5_500.png
+// @grant        none
+// ==/UserScript==
+
+
+
+window.location.replace("https://www.instagram.com/?variant=following");
